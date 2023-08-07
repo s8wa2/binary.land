0efaee5d5b5a88761b29724f8f854517fe2d31bf
s8wa2 <cgpmsuvl@gmail.com>
2023-08-07 12:40:03 -0700

    add out links

---
+++ app/routes/out.$link.tsx
@@ -0,0 +1,40 @@
+import { redirect, type LoaderArgs } from "@remix-run/node"
+import isbot from "isbot";
+import { getClientIPAddress } from "remix-utils";
+
+export async function loader({ params, request, context }: LoaderArgs) {
+  const { env } = context;
+  const { link } = params;
+  if (!link) {
+    throw new Response(null, {
+      status: 404,
+    })
+  }
+  if (typeof env !== 'object' || env === null) {
+    throw new Response(null, {
+      status: 404,
+    })
+  }
+  const agent = request.headers.get('User-Agent');
+  const isBotResult = isbot(agent);
+  const ip = getClientIPAddress(request.headers);
+  console.log({
+    resource: `/out/${link}`,
+    method: request.method,
+    ip: ip,
+    userAgent: agent,
+    isBot: isBotResult,
+  });
+  if (isBotResult) {
+    throw new Response(null, {
+      status: 404,
+    })
+  }
+  const url = (env as Record<string, string>)[`LINKS_${link.toUpperCase()}`];
+  if (!url) {
+    throw new Response(null, {
+      status: 404,
+    })
+  }
+  return redirect(url);
+};
