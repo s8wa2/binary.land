a8b6a50458a8b895cdfdf5fede3525bb0b7a6593
s8wa2 <cgpmsuvl@gmail.com>
2023-08-07 22:35:40 -0700

    Add prettier, enforce formatting, crlf -> lf

--- app/routes/out.$link.tsx
+++ app/routes/out.$link.tsx
@@ -1,6 +1,6 @@
-import { redirect, type LoaderArgs } from "@remix-run/node"
-import isbot from "isbot";
-import { getClientIPAddress } from "remix-utils";
+import { redirect, type LoaderArgs } from '@remix-run/node';
+import isbot from 'isbot';
+import { getClientIPAddress } from 'remix-utils';
 
 export async function loader({ params, request, context }: LoaderArgs) {
   const { env } = context;
@@ -8,12 +8,12 @@ export async function loader({ params, request, context }: LoaderArgs) {
   if (!link || typeof link !== 'string') {
     throw new Response(null, {
       status: 404,
-    })
+    });
   }
   if (typeof env !== 'object' || env === null) {
     throw new Response(null, {
       status: 404,
-    })
+    });
   }
   const agent = request.headers.get('User-Agent');
   const isBotResult = isbot(agent);
@@ -28,13 +28,15 @@ export async function loader({ params, request, context }: LoaderArgs) {
   if (isBotResult) {
     throw new Response(null, {
       status: 404,
-    })
+    });
   }
-  const url = (env as Record<string, string | undefined>)[`LINKS_${link.toUpperCase()}`];
+  const url = (env as Record<string, string | undefined>)[
+    `LINKS_${link.toUpperCase()}`
+  ];
   if (!url) {
     throw new Response(null, {
       status: 404,
-    })
+    });
   }
   return redirect(url);
-};
+}
