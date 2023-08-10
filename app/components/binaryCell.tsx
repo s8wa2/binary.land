b02840fa3b9d466d3b3d25c012f700319e39f73d
s8wa2 <cgpmsuvl@gmail.com>
2023-08-10 14:31:37 -0700

    Highlight cell if selected boolean

--- app/components/binaryCell.tsx
+++ app/components/binaryCell.tsx
@@ -3,6 +3,7 @@ import { MouseEventHandler } from 'react';
 export default function BinaryCell({
   bit,
   power,
+  isSelected,
   onClick,
   onMouseUp,
   onMouseEnter,
@@ -10,6 +11,7 @@ export default function BinaryCell({
 }: {
   bit: string;
   power: number;
+  isSelected?: boolean;
   onClick?: MouseEventHandler;
   onMouseUp?: MouseEventHandler;
   onMouseEnter?: MouseEventHandler;
@@ -17,23 +19,23 @@ export default function BinaryCell({
 }) {
   return (
     <div
-      className='flex flex-col items-center'
+      className='flex flex-col items-center select-none'
       onClick={onClick}
       onMouseUp={onMouseUp}
       onMouseEnter={onMouseEnter}
       onMouseDown={onMouseDown}
       style={{ direction: 'ltr' }}
     >
-      <div className='select-none flex items-baseline text-green-400'>
+      <div className='flex items-baseline text-green-400'>
         <span className='text-xs'>
           2<span className='ml-1 align-super text-xs'>{power}</span>
         </span>
       </div>
       <hr className='border-white border-[1px] w-full mt-1 mb-1 opacity-50' />
       <div
-        className={`w-10 h-10 flex items-center justify-center rounded-md mb-2 shadow-sm border-2 border-secondary  bg-transparent ${
-          bit === '1' ? 'text-bit-on' : 'text-bit-off'
-        }`}
+        className={`w-10 h-10 flex items-center justify-center rounded-md mb-2 shadow-sm border-2 border-secondary ${
+          isSelected ? 'bg-tertiary' : 'bg-transparent'
+        } ${bit === '1' ? 'text-bit-on' : 'text-bit-off'}`}
       >
         {bit}
       </div>
