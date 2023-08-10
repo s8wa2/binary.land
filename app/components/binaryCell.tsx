d860b72f95258cdcd9710fbada5945f6c7ef4ce4
s8wa2 <cgpmsuvl@gmail.com>
2023-08-09 22:14:27 -0700

    Change sup tag so it's no longer above the overlay

--- app/components/binaryCell.tsx
+++ app/components/binaryCell.tsx
@@ -3,21 +3,31 @@ import { MouseEventHandler } from 'react';
 export default function BinaryCell({
   bit,
   power,
-  onclick,
+  onClick,
+  onMouseUp,
+  onMouseEnter,
+  onMouseDown,
 }: {
   bit: string;
   power: number;
-  onclick?: MouseEventHandler;
+  onClick?: MouseEventHandler;
+  onMouseUp?: MouseEventHandler;
+  onMouseEnter?: MouseEventHandler;
+  onMouseDown?: MouseEventHandler;
 }) {
   return (
     <div
       className='flex flex-col items-center'
-      onClick={onclick}
+      onClick={onClick}
+      onMouseUp={onMouseUp}
+      onMouseEnter={onMouseEnter}
+      onMouseDown={onMouseDown}
       style={{ direction: 'ltr' }}
     >
       <div className='select-none flex items-baseline text-green-400'>
-        <span className='text-xs'>2</span>
-        <sup className='ml-1'>{power}</sup>
+        <span className='text-xs'>
+          2<span className='ml-1 align-super text-xs'>{power}</span>
+        </span>
       </div>
       <hr className='border-white border-[1px] w-full mt-1 mb-1 opacity-50' />
       <div
